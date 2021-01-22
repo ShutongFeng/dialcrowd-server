72a83bbca6da3533a1b16f86676f68a3935a92de
Hsien-Chin Lin <linh@hhu.de>
2021-01-22 11:33:48 +0100

    local path

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://dialcrowd:password@cluster0.xxx.mongodb.net/dialcrowd?retryWrites=true'
+  // mongoURI: 'mongodb+srv://dialcrowd:password@cluster0.xxx.mongodb.net/dialcrowd?retryWrites=true'
+  mongoURI: 'mongodb://127.0.0.1:27017'
 };
--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const restApi = require(path.join(__dirname, 'routes/rest_api'));
 const PORT = 3040;
 
 app.use(morgan(':remote-addr [:date] :method :url :status :res[content-length] - :response-time ms'))
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extend: false}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extend: false }));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -18,13 +18,15 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/api', restApi);
 
 // Everything else
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(function(req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', 'http://dialcrowd.sp.cs.cmu.edu')});
+// cors setting
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+});
 
-server.listen(process.env.PORT || PORT, function() {
+server.listen(process.env.PORT || PORT, function () {
   console.log("DialCrowd backend running on port " + (process.env.PORT || PORT))
 });
