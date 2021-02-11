2661bb87ace681c864405735448ae57aa21d98de
Hsien-Chin Lin <linh@hhu.de>
2021-02-11 12:19:43 +0100

    modify config

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,2 +1,2 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://blooming-forest-25223.herokuapp.com/'
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://blooming-forest-25223.herokuapp.com/'
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://blooming-forest-25223.herokuapp.com'
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://blooming-forest-25223.herokuapp.com'
