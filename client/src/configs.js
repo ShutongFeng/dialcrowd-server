ef6e6ab4d8de6b355633edf560d10e762c7f8c64
songbo <s1647079@sms.ed.ac.uk>
2021-02-20 21:03:40 +0800

    Adding SSL.

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,2 +1,10 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://blooming-forest-25223.herokuapp.com'
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://blooming-forest-25223.herokuapp.com'
+// export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://blooming-forest-25223.herokuapp.com'
+// export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://blooming-forest-25223.herokuapp.com'
+
+// export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : "http://0.0.0.0:3040"
+// export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://35.190.205.174:3040' : "http://35.190.205.174:3040"
+// export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'http://0.0.0.0:3000'
+
+
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://www.songbo.work:3040' : "http://www.songbo.work:3040"
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://www.songbo.work:3000' : 'http://www.songbo.work:3000'
