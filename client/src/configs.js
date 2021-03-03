eb4372e3893224ec6cfd96c8b9cff8831e3174c5
Hsien-Chin Lin <linh@hhu.de>
2021-03-03 18:57:14 +0100

    fix config

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,2 +1,2 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://dialeval.cs.hhu.de/'
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialeval.cs.hhu.de/'
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://dialeval.cs.hhu.de'
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialeval.cs.hhu.de'
