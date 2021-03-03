d6c5562291d99156ee8c7655adb594f185f9386e
Hsien-Chin Lin <linh@hhu.de>
2021-03-03 11:21:40 +0100

    change mongodb-path

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // mongoURI: 'mongodb+srv://dialcrowd:password@cluster0.xxx.mongodb.net/dialcrowd?retryWrites=true'
-  mongoURI: 'mongodb://34.76.192.20:27027'
+  mongoURI: 'mongodb://34.78.248.98:27027'
 };
