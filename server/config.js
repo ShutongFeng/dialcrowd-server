ebde515f0bb53bd74f4efd956fe17d5dc0d09f4a
Hsien-Chin Lin <linh@hhu.de>
2022-01-03 16:17:11 +0100

    update package. move mongo url to env

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // mongoURI: 'mongodb+srv://dialcrowd:password@cluster0.xxx.mongodb.net/dialcrowd?retryWrites=true'
-  mongoURI: 'mongodb://34.78.248.98:27027'
+  mongoURI: "mongodb://" + process.env.mongoURI + "/dialcrowd",
 };
