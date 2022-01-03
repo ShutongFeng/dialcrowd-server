5b166e7db1afb9f91f911d94cd7a58168f489193
Hsien-Chin Lin <linh@hhu.de>
2022-01-03 17:23:49 +0100

    add auth control

--- server/config.js
+++ server/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // mongoURI: 'mongodb+srv://dialcrowd:password@cluster0.xxx.mongodb.net/dialcrowd?retryWrites=true'
-  mongoURI: "mongodb://" + process.env.mongoURI + "/dialcrowd",
+  mongoURI:
+    "mongodb://" +
+    process.env.mongoUserName +
+    ":" +
+    process.env.mongoPWD +
+    "@" +
+    process.env.mongoURI +
+    "/?retryWrites=true",
 };
