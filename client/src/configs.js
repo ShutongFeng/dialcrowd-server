d9e0db1182b246f2bbafb0b0539badf6b98a87d1
Hsien-Chin Lin <linh@hhu.de>
2023-11-08 00:22:35 +0100

    update url

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,3 +1,4 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://dialeval-dev.herokuapp.com'
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialeval-dev.herokuapp.com'
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://dialcorwdv2.vercel.app'
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialcorwdv2.vercel.app
+'
 
