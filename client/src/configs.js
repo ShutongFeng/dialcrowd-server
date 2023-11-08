36569620fe75f32aafaae02e8c7808a016986dee
Hsien-Chin Lin <linh@hhu.de>
2023-11-08 10:48:43 +0100

    fix typo

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,4 +1,3 @@
 export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3040' : 'https://dialcorwdv2.vercel.app'
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialcorwdv2.vercel.app
-'
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : 'https://dialcorwdv2.vercel.app'
 
