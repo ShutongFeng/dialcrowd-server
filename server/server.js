add9503a99216eff0b1654fb2a862279c80e0c7e
Carel van Niekerk <vniekerk.carel@gmail.com>
2024-02-08 11:45:01 +0100

    Get frontend URL for CORS from env

--- server/server.js
+++ server/server.js
@@ -24,13 +24,8 @@ app.get('*', function (req, res) {
 });
 
 // cors setting TODO set to https://dialeval.cs.hhu.de?
-app.use(cors());
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', 'https://dialcrowdv4-alpha.vercel.app');
-res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
-res.header('Access-Control-Allow-Credentials', 'true');
-next();
+res.header('Access-Control-Allow-Origin', process.env.frontEndURL)
 });
 
 server.listen(process.env.PORT || PORT, function () {
